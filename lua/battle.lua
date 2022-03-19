d42e01990ebb174970a290daaa5fd0026b534504
bigfarts <bigfarts@punymail.com>
2022-03-19 12:14:26 -0700

    found the custom init data structure. closes #2

--- lua/battle.lua
+++ lua/battle.lua
@@ -12,52 +12,32 @@ local function set_p2_input(joyflags)
     memory.write_u16(0x0200948a, joyflags)
 end
 
-local function get_p1_hand(hand)
-    return memory.read_range(0x020349c0, 0x50)
+-- TODO: Find where custom screen init is committed.
+
+local function get_p1_custominit(custominit)
+    return memory.read_range(0x0203f4a0, 0x100)
 end
 
-local function set_p1_hand(hand)
-    assert(#hand == 0x50)
-    memory.write_range(0x020349c0, hand)
+local function set_p1_custominit(custominit)
+    assert(#custominit == 0x100)
+    memory.write_range(0x0203f4a0, custominit)
 end
 
-local function get_p2_hand(hand)
-    return memory.read_range(0x02034a10, 0x50)
+local function get_p2_custominit(custominit)
+    return memory.read_range(0x0203f5a0, 0x100)
 end
 
-local function set_p2_hand(hand)
-    assert(#hand == 0x50)
-    memory.write_range(0x02034a10, hand)
-end
-
-local function get_p1_navistats(navistats)
-    return memory.read_range(0x0203ce00, 0x64)
-end
-
-local function set_p1_navistats(navistats)
-    assert(#navistats == 0x64)
-    memory.write_range(0x0203ce00, navistats)
-end
-
-local function get_p2_navistats(navistats)
-    return memory.read_range(0x0203ce64, 0x64)
-end
-
-local function set_p2_navistats(navistats)
-    assert(#navistats == 0x64)
-    memory.write_range(0x0203ce64, navistats)
+local function set_p2_custominit(custominit)
+    assert(#custominit == 0x100)
+    memory.write_range(0x0203f5a0, custominit)
 end
 
 return {
     set_battle_rng = set_battle_rng,
     set_p1_input = set_p1_input,
     set_p2_input = set_p2_input,
-    get_p1_hand = get_p1_hand,
-    set_p1_hand = set_p1_hand,
-    get_p2_hand = get_p2_hand,
-    set_p2_hand = set_p2_hand,
-    get_p1_navistats = get_p1_navistats,
-    set_p1_navistats = set_p1_navistats,
-    get_p2_navistats = get_p2_navistats,
-    set_p2_navistats = set_p2_navistats,
+    get_p1_custominit = get_p1_custominit,
+    set_p1_custominit = set_p1_custominit,
+    get_p2_custominit = get_p2_custominit,
+    set_p2_custominit = set_p2_custominit,
 }
