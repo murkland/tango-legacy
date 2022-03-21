6b62f3f77d48bbf0760311ad0f4e354a933d3fa4
bigfarts <bigfarts@punymail.com>
2022-03-20 22:56:15 -0700

    add battle settings list pointer. #5

--- lua/romoffsets.lua
+++ lua/romoffsets.lua
@@ -10,6 +10,7 @@ local offsets = {
         battle_init__call__battle_copyInputData                     = 0x08007902,
         battle_update__call__battle_copyInputData                   = 0x08007a6e,
         battle_getSettingsForLink__ret                              = 0x0802d27e,
+        battle_settings_list1                                       = 0x080b0d88,
         battle_init_marshal__ret                                    = 0x0800b2b8,
         battle_turn_marshal__ret                                    = 0x0800b3d6,
         battle_updating__ret__go_to_custom_screen                   = 0x080093ae,
