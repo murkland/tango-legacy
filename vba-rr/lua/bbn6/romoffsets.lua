8c561885242104a7f02533450cac46b3e15bab32
bigfarts <bigfarts@punymail.com>
2022-03-21 10:07:27 -0700

    wild guesses

--- vba-rr/lua/bbn6/romoffsets.lua
+++ vba-rr/lua/bbn6/romoffsets.lua
@@ -20,6 +20,23 @@ local offsets = {
         commMenu_inBattle__call__commMenu_handleLinkCableInput      = 0x0812b5ca,
         battle_isRemote__ret                                        = 0x0803dd52,
         link_isRemote__ret                                          = 0x0803dd86,
+    },
+
+    MEGAMAN6_GXX = {
+        commMenu_handleLinkCableInput__entry                        = 0x0803eaae,
+        battle_init__call__battle_copyInputData                     = 0x080078f8,
+        battle_update__call__battle_copyInputData                   = 0x08007a64,
+        battle_getSettingsForLink__ret                              = 0x0802d274,
+        battle_settings_list1                                       = 0x080b25ee,
+        battle_init_marshal__ret                                    = 0x0800b2ae,
+        battle_turn_marshal__ret                                    = 0x0800b3cc,
+        battle_updating__ret__go_to_custom_screen                   = 0x080093a4,
+        battle_start__ret                                           = 0x080072fa,
+        battle_end__entry                                           = 0x08007c96,
+        commMenu_waitForFriend__call__commMenu_handleLinkCableInput = 0x0812bd6a,
+        commMenu_inBattle__call__commMenu_handleLinkCableInput      = 0x0812d3aa,
+        battle_isRemote__ret                                        = 0x0803dd1c,
+        link_isRemote__ret                                          = 0x0803dd50,
     }
 }
 
