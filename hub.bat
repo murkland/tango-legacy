cff66aacd23b02b8171e2bba1019abee22555570
Cristian Silva Silva <43609215+ChocolatCat@users.noreply.github.com>
2022-03-24 14:20:26 -0300

    Fixed cute ASCII art. Prettified messages.
    --Fixed Hub art (non-standard ASCII set)
    --Prettified server-client messages
    --Changed logfiles to hub_server.log and hub_client.log

--- hub.bat
+++ hub.bat
@@ -1,34 +1,34 @@
 @echo off
 cls
 :MENU
-ECHO.   .  .°O*.  ..°...°°..°*°*O#Oo*°°..°*.°°°°     .#o     
-ECHO.   °  .°O*.  ..*°.°*°.°***oO##o***°.°*.°*°°     .#O     
-ECHO.  .°.   O°.   °**.oo°.°oo*oO##Ooo*°°**.*O.°.   ..#o     
-ECHO. ..*.   O. ...°**.*o°°°ooooO##Ooo***o*°oo.o°.  ..O.     
-ECHO. .°*.  .°. ...*OO°oO**°oooOO##Ooooo*oooOO*O°°....O.     
-ECHO. .°O.  ......°OOO**ooooOOOO#O**°.°°°°°oOooo*°°°°.°.     
-ECHO. .*#......°°°*oOOooOOooo****°°°°.°°*°**oOoooo***°°°°  . 
-ECHO. .*#. ..°°°°**oOOoOo*°°°°°°°°°°°°°**ooo*...**Oo*°°°°  ° 
-ECHO. .*O....°°°**ooO#Oo***********oooooo**o* ..°°°*o°.... O 
-ECHO. .°O...°°*o*ooOO**oooo*o*oo*o**o***°***..°*°*°°*o*°°° # 
-ECHO. .°*....°*o*oO*°°°*****°°*°°°..°°°°°*oo.....°°..°o*°°.* 
-ECHO.  ..°.°°*oOooO********°°°***°.°****°*##O*°°.°**o..o°..° 
-ECHO.  .....**oOoO°.°°°°°°..°°°°...°°°°°*O#OOOo ...°*# °*... 
-ECHO. ...°°***O#oO°.°..°°.°**°°°°°*****o°°O#O#O°°.°.°*O.O°°° 
-ECHO.  °°°°*oOO#O#°...*°..°°.°°°*°°°*°°**.°oOO##°..°.*OO*..° 
-ECHO. .°°°o***oooo*° ***o°.°******oO*oo.Oo*O##o**...°°Oo*°*° 
-ECHO.  ...°o°*....°. *oooo*°..°ooOO#O#o.*#OOo°.o .* .Oo*°.O. 
-ECHO. .°°°°*##oo°.....*oo***..oOOoOOOOOoOOOO* °*.*O oOo*.°#° 
-ECHO.  .°°°°Oo°°.......°*ooo*°*ooo#OOO###OO#Oo..o#Oo#Ooo°*#* 
-ECHO. ..°°°*OOOOooo*°..°°°***°..*OOOOOOOOOOO#OO#####Oooo°*#* 
-ECHO.  o°.°*ooOO#o*. ... .      .o#O#OOOOO##**###OOOOooo°°#° 
-ECHO.  o°°**OOOOOoooo#°.*#°      .o#OO#OOOo*o###OOOOoo**..#  
-ECHO.  O..*.oOoO##OOo##O#@°        °oOOOooO####OOO#Ooo** .O  
-ECHO.  O°.O°*ooOO*..°°o#O°.. .      °OOO####O#O##OOooo*°  °  
-ECHO.  O°.O°°oo#°.°**°°*°°°°°.....  .*O####O##O##oOooO*°  .  
-ECHO.  O°.#°.oo#o..°°°**oOOOO*........*Oo##OO#OO#oOooO*.  .  
-ECHO.  O°*#*.ooO#O°.***oOO##OO°  ......°°*OOO#Oo#oOooO*.     
-ECHO.  O.O#*.*o*O#*°**oOooOOoO*..    ....°°oO#oo#oOooO°.    °
+ECHO.   .  .-O*.  ..-...--..-*-*O#Oo*--..-*.----     .#o     
+ECHO.   -  .-O*.  ..*-.-*-.-***oO##o***-.-*.-*--     .#O     
+ECHO.  .-.   O-.   -**.oo-.-oo*oO##Ooo*--**.*O.-.   ..#o     
+ECHO. ..*.   O. ...-**.*o---ooooO##Ooo***o*-oo.o-.  ..O.     
+ECHO. .-*.  .-. ...*OO-oO**-oooOO##Ooooo*oooOO*O--....O.     
+ECHO. .-O.  ......-OOO**ooooOOOO#O**-.-----oOooo*----.-.     
+ECHO. .*#......---*oOOooOOooo****----.--*-**oOoooo***----  . 
+ECHO. .*#. ..----**oOOoOo*-------------**ooo*...**Oo*----  - 
+ECHO. .*O....---**ooO#Oo***********oooooo**o* ..---*o-.... O 
+ECHO. .-O...--*o*ooOO**oooo*o*oo*o**o***-***..-*-*--*o*--- # 
+ECHO. .-*....-*o*oO*---*****--*---..-----*oo.....--..-o*--.* 
+ECHO.  ..-.--*oOooO********---***-.-****-*##O*--.-**o..o-..- 
+ECHO.  .....**oOoO-.------..----...-----*O#OOOo ...-*# -*... 
+ECHO. ...--***O#oO-.-..--.-**-----*****o--O#O#O--.-.-*O.O--- 
+ECHO.  ----*oOO#O#-...*-..--.---*---*--**.-oOO##-..-.*OO*..- 
+ECHO. .---o***oooo*- ***o-.-******oO*oo.Oo*O##o**...--Oo*-*- 
+ECHO.  ...-o-*....-. *oooo*-..-ooOO#O#o.*#OOo-.o .* .Oo*-.O. 
+ECHO. .----*##oo-.....*oo***..oOOoOOOOOoOOOO* -*.*O oOo*.-#- 
+ECHO.  .----Oo--.......-*ooo*-*ooo#OOO###OO#Oo..o#Oo#Ooo-*#* 
+ECHO. ..---*OOOOooo*-..---***-..*OOOOOOOOOOO#OO#####Oooo-*#* 
+ECHO.  o-.-*ooOO#o*. ... .      .o#O#OOOOO##**###OOOOooo--#- 
+ECHO.  o--**OOOOOoooo#-.*#-      .o#OO#OOOo*o###OOOOoo**..#  
+ECHO.  O..*.oOoO##OOo##O#@-        -oOOOooO####OOO#Ooo** .O  
+ECHO.  O-.O-*ooOO*..--o#O-.. .      -OOO####O#O##OOooo*-  -  
+ECHO.  O-.O--oo#-.-**--*-----.....  .*O####O##O##oOooO*-  .  
+ECHO.  O-.#-.oo#o..---**oOOOO*........*Oo##OO#OO#oOooO*.  .  
+ECHO.  O-*#*.ooO#O-.***oOO##OO-  ......--*OOO#Oo#oOooO*.     
+ECHO.  O.O#*.*o*O#*-**oOooOOoO*..    ....--oO#oo#oOooO-.    -
 ECHO .........................................................
 ECHO    Welcome to BBN6 - Hub Batch is Prettier Now Edition
 ECHO .........................................................
@@ -45,7 +45,7 @@ echo:
 :SERVER
 echo Welcome to the BBN6 Server Setup!
 set /p "SESSION_ID=matchmaking code (no spaces, pick one you and your friend agree on!): "
-bbn6.exe -connect_addr=http://167.71.122.211:11223 -session_id=%SESSION_ID% 2> bbn6_server.log
+bbn6.exe -connect_addr=http://167.71.122.211:11223 -session_id=%SESSION_ID% 2> hub_server.log
 pause
 GOTO EOF
 echo:
@@ -53,7 +53,8 @@ echo:
 @echo off
 echo Welcome to the BBN6 Client Setup!
 set /p "SESSION_ID=matchmaking code (no spaces, pick one you and your friend agree on!): "
-bbn6.exe -connect_addr=http://167.71.122.211:11223 -answer -session_id=%SESSION_ID% 2> bbn6_client.log
+bbn6.exe -connect_addr=http://167.71.122.211:11223 -answer -session_id=%SESSION_ID% 2> hub_client.log
 pause
 GOTO EOF
 echo:
+
