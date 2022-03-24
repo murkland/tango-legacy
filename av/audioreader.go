4d3b96349b28451be94bc2d4336bc8ce4c9379fe
bigfarts <bigfarts@punymail.com>
2022-03-23 18:27:54 -0700

    fix windows build

--- av/audioreader.go
+++ av/audioreader.go
@@ -8,7 +8,7 @@ import (
 )
 
 /*
-#import <stdlib.h>
+#include <stdlib.h>
 */
 import "C"
 
---
+++ mgba/mod_windows.go
@@ -0,0 +1,8 @@
+//go:build windows
+
+package mgba
+
+/*
+#cgo LDFLAGS: -lshlwapi -lole32 -luuid
+*/
+import "C"
