20439fefb5a28804617bad40a9021e12e9c3b5a0
bigfarts <bigfarts@punymail.com>
2022-03-24 15:56:02 -0700

    fix hwm

--- game/game.go
+++ game/game.go
@@ -410,8 +410,7 @@ func (g *Game) Update() error {
 		g.battle.mu.Lock()
 		defer g.battle.mu.Unlock()
 
-		// TODO: Should this be different?
-		highWaterMark := int(g.medianDelay()*time.Duration(g.mainCore.GBA().Sync().FPSTarget())/time.Second + 1)
+		highWaterMark := int(g.medianDelay()*time.Duration(g.mainCore.GBA().Sync().FPSTarget())/2/time.Second + 1)
 		if highWaterMark < 1 {
 			highWaterMark = 1
 		}
