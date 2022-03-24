4d00eb8ee8e9f70fe2967df873d223d2d59ca1bf
Cristian Silva Silva <43609215+ChocolatCat@users.noreply.github.com>
2022-03-24 13:57:34 -0300

    Added trailing newlines per request

--- hub.bat
+++ hub.bat
@@ -13,14 +13,14 @@ SET /P M=Type 1, 2, or 3 then press ENTER:
 IF %M%==1 GOTO SERVER
 IF %M%==2 GOTO CLIENT
 IF %M%==3 GOTO EOF
-
+echo:
 :SERVER
 echo welcome to the bbn6 server!
 set /p "SESSION_ID=matchmaking code (no spaces, pick one you and your friend agree on!): "
 bbn6.exe -connect_addr=http://167.71.122.211:11223 -session_id=%SESSION_ID% 2> bbn6_server.log
 pause
 GOTO EOF
-
+echo:
 :CLIENT
 @echo off
 echo welcome to the bbn6 client!
@@ -28,5 +28,6 @@ set /p "SESSION_ID=matchmaking code (no spaces, pick one you and your friend agr
 bbn6.exe -connect_addr=http://167.71.122.211:11223 -answer -session_id=%SESSION_ID% 2> bbn6_client.log
 pause
 GOTO EOF
-
+echo:
 :EOF
+echo:
