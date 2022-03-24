8afe920d83b4d99d818476fbb2f3b1dd6fec8287
Cristian Silva Silva <43609215+ChocolatCat@users.noreply.github.com>
2022-03-24 14:11:53 -0300

    Prettified Hub

--- hub.bat
+++ hub.bat
@@ -1,9 +1,38 @@
 @echo off
+cls
 :MENU
 ECHO.
-ECHO ...................
-ECHO  Welcome to BBN6
-ECHO ...................
+ECHO.   .  .°O*.  ..°...°°..°*°*O#Oo*°°..°*.°°°°     .#o     
+ECHO.   °  .°O*.  ..*°.°*°.°***oO##o***°.°*.°*°°     .#O     
+ECHO.  .°.   O°.   °**.oo°.°oo*oO##Ooo*°°**.*O.°.   ..#o     
+ECHO. ..*.   O. ...°**.*o°°°ooooO##Ooo***o*°oo.o°.  ..O.     
+ECHO. .°*.  .°. ...*OO°oO**°oooOO##Ooooo*oooOO*O°°....O.     
+ECHO. .°O.  ......°OOO**ooooOOOO#O**°.°°°°°oOooo*°°°°.°.     
+ECHO. .*#......°°°*oOOooOOooo****°°°°.°°*°**oOoooo***°°°°  . 
+ECHO. .*#. ..°°°°**oOOoOo*°°°°°°°°°°°°°**ooo*...**Oo*°°°°  ° 
+ECHO. .*O....°°°**ooO#Oo***********oooooo**o* ..°°°*o°.... O 
+ECHO. .°O...°°*o*ooOO**oooo*o*oo*o**o***°***..°*°*°°*o*°°° # 
+ECHO. .°*....°*o*oO*°°°*****°°*°°°..°°°°°*oo.....°°..°o*°°.* 
+ECHO.  ..°.°°*oOooO********°°°***°.°****°*##O*°°.°**o..o°..° 
+ECHO.  .....**oOoO°.°°°°°°..°°°°...°°°°°*O#OOOo ...°*# °*... 
+ECHO. ...°°***O#oO°.°..°°.°**°°°°°*****o°°O#O#O°°.°.°*O.O°°° 
+ECHO.  °°°°*oOO#O#°...*°..°°.°°°*°°°*°°**.°oOO##°..°.*OO*..° 
+ECHO. .°°°o***oooo*° ***o°.°******oO*oo.Oo*O##o**...°°Oo*°*° 
+ECHO.  ...°o°*....°. *oooo*°..°ooOO#O#o.*#OOo°.o .* .Oo*°.O. 
+ECHO. .°°°°*##oo°.....*oo***..oOOoOOOOOoOOOO* °*.*O oOo*.°#° 
+ECHO.  .°°°°Oo°°.......°*ooo*°*ooo#OOO###OO#Oo..o#Oo#Ooo°*#* 
+ECHO. ..°°°*OOOOooo*°..°°°***°..*OOOOOOOOOOO#OO#####Oooo°*#* 
+ECHO.  o°.°*ooOO#o*. ... .      .o#O#OOOOO##**###OOOOooo°°#° 
+ECHO.  o°°**OOOOOoooo#°.*#°      .o#OO#OOOo*o###OOOOoo**..#  
+ECHO.  O..*.oOoO##OOo##O#@°        °oOOOooO####OOO#Ooo** .O  
+ECHO.  O°.O°*ooOO*..°°o#O°.. .      °OOO####O#O##OOooo*°  °  
+ECHO.  O°.O°°oo#°.°**°°*°°°°°.....  .*O####O##O##oOooO*°  .  
+ECHO.  O°.#°.oo#o..°°°**oOOOO*........*Oo##OO#OO#oOooO*.  .  
+ECHO.  O°*#*.ooO#O°.***oOO##OO°  ......°°*OOO#Oo#oOooO*.     
+ECHO.  O.O#*.*o*O#*°**oOooOOoO*..    ....°°oO#oo#oOooO°.    °
+ECHO .........................................................
+ECHO    Welcome to BBN6 - Hub Batch is Prettier Now Edition
+ECHO .........................................................
 ECHO.
 ECHO 1 - Be Server
 ECHO 2 - Be Client
