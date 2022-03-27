0ba7b72b0f5dc767e9e8906c19d99b244f7d3b9f
bigfarts <bigfarts@punymail.com>
2022-03-27 16:54:02 -0700

    add mgba config setting

--- mgba/config.go
+++ mgba/config.go
@@ -23,3 +23,20 @@ func (c *Config) Init(name string) {
 func (c *Config) Deinit() {
 	C.mCoreConfigLoad(c.ptr)
 }
+
+func (c *Config) SetValue(key string, value string) {
+	keyCstr := C.CString(key)
+	defer C.free(unsafe.Pointer(keyCstr))
+
+	valueCstr := C.CString(value)
+	defer C.free(unsafe.Pointer(valueCstr))
+
+	C.mCoreConfigSetValue(c.ptr, keyCstr, valueCstr)
+}
+
+func (c *Config) Value(key string) string {
+	keyCstr := C.CString(key)
+	defer C.free(unsafe.Pointer(keyCstr))
+
+	return C.GoString(C.mCoreConfigGetValue(c.ptr, keyCstr))
+}
