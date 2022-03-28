941a8db92582600b102ce0d11412646cd4bb5436
bigfarts <bigfarts@punymail.com>
2022-03-27 19:26:50 -0700

    shorten some text

--- game/debugspew.go
+++ game/debugspew.go
@@ -46,7 +46,7 @@ func (g *Game) spewDebug(screen *ebiten.Image) {
 	if g.match != nil && g.match.battle != nil {
 		lines = append(lines,
 			fmt.Sprintf("is p2:   %t", g.match.battle.isP2),
-			fmt.Sprintf("inputq:  %2d vs %2d (max %2d)", g.match.battle.iq.qs[g.match.battle.LocalPlayerIndex()].Used(), g.match.battle.iq.qs[g.match.battle.RemotePlayerIndex()].Used(), g.runaheadTicksAllowed()),
+			fmt.Sprintf("inputq:  %2d:%2d (max %2d)", g.match.battle.iq.qs[g.match.battle.LocalPlayerIndex()].Used(), g.match.battle.iq.qs[g.match.battle.RemotePlayerIndex()].Used(), g.runaheadTicksAllowed()),
 		)
 	}
 	text.Draw(screen, strings.Join(lines, "\n"), mplusNormalFont, 2, 14, color.RGBA{0xff, 0x00, 0xff, 0xff})
