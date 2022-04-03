1c1e06f6b7a3643c61647248a74ba190863708ed
bigfarts <bigfarts@punymail.com>
2022-04-03 10:41:32 -0700

    Revert "build in i686 for those with cursed computers"
    This reverts commit f956b0f113598de02918d4689449a6f27545acfe.

--- mgba/Toolchain-x86_64-w64-mingw32.cmake
+++ mgba/Toolchain-x86_64-w64-mingw32.cmake
@@ -1,10 +1,10 @@
 set(CMAKE_SYSTEM_NAME Windows)
 
 # which compilers to use for C and C++
-SET(CMAKE_C_COMPILER     "i686-w64-mingw32-gcc")
-SET(CMAKE_CXX_COMPILER   "i686-w64-mingw32-g++")
-SET(CMAKE_RC_COMPILER    "i686-w64-mingw32-windres")
-SET(CMAKE_RANLIB         "i686-w64-mingw32-ranlib")
+SET(CMAKE_C_COMPILER     "x86_64-w64-mingw32-gcc")
+SET(CMAKE_CXX_COMPILER   "x86_64-w64-mingw32-g++")
+SET(CMAKE_RC_COMPILER    "x86_64-w64-mingw32-windres")
+SET(CMAKE_RANLIB         "x86_64-w64-mingw32-ranlib")
 
 # where is the target environment located
 set(CMAKE_FIND_ROOT_PATH  "/usr/bin")
