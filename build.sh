d44c54c54ab0a6da8d7cb502b805bcf9b1c0aaa6
bigfarts <bigfarts@punymail.com>
2022-04-04 00:28:57 -0700

    set -euxo pipefail

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euxo pipefail
 go-winres make --product-version=git-tag
 go build -ldflags "-X main.version=$(git describe --tags)$(test -n "$(git status --porcelain)" && echo '-dirty')" -trimpath -o build/ -ldflags "-H windowsgui" .
 go build -ldflags "-X main.version=$(git describe --tags)$(test -n "$(git status --porcelain)" && echo '-dirty')" -trimpath -o build/ ./tools/replayview
