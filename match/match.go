6660942909e7e12e22424a36b475eb817066e13f
bigfarts <bigfarts@punymail.com>
2022-04-04 10:33:28 -0700

    drop excess inputs

--- match/match.go
+++ match/match.go
@@ -207,6 +207,10 @@ func (m *Match) handleConn(ctx context.Context) error {
 			}
 		case packets.Input:
 			battle := m.Battle()
+			if battle == nil {
+				log.Printf("no battle in progress, dropping input")
+				continue
+			}
 			if p.BattleNumber != uint8(battle.number) {
 				log.Printf("mismatched battle number, expected %d but got %d, droopping packet", battle.number, p.BattleNumber)
 				continue
