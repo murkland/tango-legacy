1e2f71c44304ea1707e4b3990e9a2b9b9a108129
bigfarts <bigfarts@punymail.com>
2022-04-05 01:10:11 -0700

    fix ldflags

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euxo pipefail
 go-winres make --product-version=git-tag
-go build -ldflags "-X main.version=$(git describe --tags)$(test -n "$(git status --porcelain)" && echo '-dirty')" -trimpath -o build/ -ldflags "-H windowsgui" .
+go build -ldflags "-X main.version=$(git describe --tags)$(test -n "$(git status --porcelain) -H windowsgui" && echo '-dirty')" -trimpath -o build/ .
 go build -ldflags "-X main.version=$(git describe --tags)$(test -n "$(git status --porcelain)" && echo '-dirty')" -trimpath -o build/ ./tools/replayview
