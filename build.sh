b3f1c7d629af62c50e0f009ef6dbc48fec372528
bigfarts <bigfarts@punymail.com>
2022-04-10 23:10:50 -0700

    minor fix

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euxo pipefail
 go-winres make --product-version=git-tag
-go build -ldflags "-X main.version=$(git describe --tags)$(test -n "$(git status --porcelain) -H windowsgui" && echo '-dirty')" -trimpath -o build/ .
+go build -ldflags "-X main.version=$(git describe --tags)$(test -n "$(git status --porcelain)" && echo '-dirty') -H windowsgui" -trimpath -o build/ .
 go build -ldflags "-X main.version=$(git describe --tags)$(test -n "$(git status --porcelain)" && echo '-dirty')" -trimpath -o build/ ./tools/replayview
